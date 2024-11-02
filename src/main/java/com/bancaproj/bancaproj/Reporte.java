20b10e6866f647cd45e08c8a415db53ac322a4c7
Wilfredodiazalguera <wilfredodiazalguera@gmail.com>
2024-11-02 17:37:22 -0600

    Adding Reporte Class

---
+++ src/main/java/com/bancaproj/bancaproj/Reporte.java
@@ -0,0 +1,16 @@
+package com.bancaproj.bancaproj;
+
+
+public class Reporte {
+
+    public Reporte() {
+    }
+    
+    public void reporteCuentasActivas(){}
+    
+    public void reporteTransaccionesCuenta(){}
+    
+    public void eportePrestamos(){}
+    
+}
+
