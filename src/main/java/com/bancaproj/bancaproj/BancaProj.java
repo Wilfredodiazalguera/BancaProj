29bff5d269ea199116a9c42d4f779f554a50f41d
Wilfredodiazalguera <wilfredodiazalguera@gmail.com>
2024-11-02 14:33:31 -0600

    Cleaning code
    Cleaning code

--- src/main/java/com/bancaproj/bancaproj/BancaProj.java
+++ src/main/java/com/bancaproj/bancaproj/BancaProj.java
@@ -1,16 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.bancaproj.bancaproj;
 
-/**
- *
- * @author willdiaz
- */
 public class BancaProj {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
     }
 }
