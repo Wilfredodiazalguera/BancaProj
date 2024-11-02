d1d3ac3fe4ef9c412696dc49f68a101dc725a7f2
Wilfredodiazalguera <wilfredodiazalguera@gmail.com>
2024-11-02 14:30:52 -0600

    Initial commit

---
+++ src/main/java/com/bancaproj/bancaproj/BancaProj.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.bancaproj.bancaproj;
+
+/**
+ *
+ * @author willdiaz
+ */
+public class BancaProj {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
