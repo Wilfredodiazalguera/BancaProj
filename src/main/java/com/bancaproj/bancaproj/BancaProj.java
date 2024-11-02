0e97fa3c28c883b42d99dbc213d97e71b5e8522c
Wilfredodiazalguera <wilfredodiazalguera@gmail.com>
2024-11-02 14:52:20 -0600

    Update test
    Update test

--- src/main/java/com/bancaproj/bancaproj/BancaProj.java
+++ src/main/java/com/bancaproj/bancaproj/BancaProj.java
@@ -3,6 +3,6 @@ package com.bancaproj.bancaproj;
 public class BancaProj {
 
     public static void main(String[] args) {
-        
+        System.out.println("com.bancaproj.bancaproj.BancaProj.main()");
     }
 }
