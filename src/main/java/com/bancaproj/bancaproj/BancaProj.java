856764610671ba0d940b1dffb4d2da8491cfa8c4
Wilfredodiazalguera <wilfredodiazalguera@gmail.com>
2024-11-02 15:06:30 -0600

    Creacion default de menu principal

--- src/main/java/com/bancaproj/bancaproj/BancaProj.java
+++ src/main/java/com/bancaproj/bancaproj/BancaProj.java
@@ -1,8 +1,32 @@
 package com.bancaproj.bancaproj;
 
+import javax.swing.JOptionPane;
+
 public class BancaProj {
 
     public static void main(String[] args) {
-        System.out.println("com.bancaproj.bancaproj.BancaProj.main()");
+        int opcion = 0;
+        String textoMenu = "1. Registro de cuentas bancarias\n"
+                + "2. Proceso de Transacciones\n"
+                + "3. Registro de préstamos\n"
+                + "4. Módulo de reportes.\n";
+        while (opcion != 4) {
+            opcion = Integer.parseInt(JOptionPane.showInputDialog(textoMenu));
+
+            switch (opcion) {
+                case 1:
+                
+                    break;
+                case 2:
+                   
+                    break;
+                case 3:
+                   
+                case 4:
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "");
+            };
+        };
     }
 }
