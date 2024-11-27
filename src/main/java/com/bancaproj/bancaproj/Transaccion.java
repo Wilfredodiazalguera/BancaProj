7e16a9e29efc9d6a2a30b86dc1c882492e807b5b
Shei203 <sheilafallasm02@gmail.com>
2024-11-26 20:16:42 -0600

    Opciones retiro
    Avance de las opciones de transferencia

--- src/main/java/com/bancaproj/bancaproj/Transaccion.java
+++ src/main/java/com/bancaproj/bancaproj/Transaccion.java
@@ -7,34 +7,63 @@ public class Transaccion {
     //Pending; Crear variable Array static totalTransacciones
     private Cuenta cuenta;
     private Cuenta cuentaDestino;
+    private Cuenta cuentaOrigen;
     private double monto;
+    private String tipoTransaccion;
 
     public Transaccion() {
     }
+    //j
 
-    public Transaccion(Cuenta cuenta, Cuenta cuentaDestino, double monto) {
-        setCuenta(cuenta);
-        setCuentaDestino(cuentaDestino);
-        setMonto(monto);
+    public Transaccion(Cuenta cuentaOrigen, Cuenta cuentaDestino, double monto, String tipoTransaccion) {
+        this.cuentaOrigen = cuentaOrigen;
+        this.cuentaDestino = cuentaDestino;
+        this.monto = monto;
+        this.tipoTransaccion = tipoTransaccion;
     }
-    
-    public static void realizarTransacciones(){
+
+    public static void realizarTransacciones() {
         int opcion = 0;
         String menuTransacciones = "Seleccione el tipo de transaccion\n"
                 + "1. Deposito\n"
                 + "2. Retiro\n"
                 + "3. Transferencia\n";
-        
+
         int tipoTransaccion = Integer.parseInt(JOptionPane.showInputDialog(menuTransacciones));
-        switch(tipoTransaccion){
+        switch (tipoTransaccion) {
             case 1: //Deposito
-                int cuentaDestino = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero de cuenta destino;"));
+                realizarDeposito();
                 break;
             case 2: //Retiro
+                realizarRetiro();
                 break;
             case 3: //Transferencia
+                realizarTransferencia();
                 break;
         }
+    }
+
+    //Metodos
+    public static void realizarDeposito() {
+        double monto = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el monto a depositar:"));
+        if (monto <= 0) {
+            JOptionPane.showMessageDialog(null, "Monto inválido.");
+            return;
+        }
+    }
+
+    public static void realizarRetiro() {
+        double monto = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el monto a depositar:"));
+        if (monto <= 0) {
+            JOptionPane.showMessageDialog(null, "Monto inválido.");
+            return;
+        }
+    }
+
+    public static void realizarTransferencia() {
+    }
+
+    public void registrarTransaccion() {
     }
 
     //Getter
@@ -61,19 +90,6 @@ public class Transaccion {
 
     public void setMonto(double monto) {
         this.monto = monto;
-    }
-
-    //Metodos
-    public void depositar() {
-    }
-
-    public void retirar() {
-    }
-
-    public void transferir(int cuentaDestino) {
-    }
-
-    public void registrarTransaccion() {
     }
 
 }
